fc87f42870a7fc8e7debeae027cbf2228254d432
Charutha Bandara <charuthabandara@gmail.com>
2019-02-01 17:47:46 -0500

    configured mlab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://<dbuser>:<dbpassword>@ds037990.mlab.com:37990/blogposts'
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blog-app-test'
 exports.PORT = process.env.PORT || 8080;
